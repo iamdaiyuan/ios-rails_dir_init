63731562ab02ffb0967616a0dc9713f939d0fc53
shiren1118 <shiren1118@126.com>
2013-03-13 14:42:51 +0800

    only ideas

---
+++ README.md
@@ -0,0 +1,79 @@
+
+# rails-like ios project dir practice
+
+//  rails-like ios project dir practice
+//  create ios project dir like rails dir
+//
+//  Created by sang alfred on 2013-03-13.
+//  Copyright (c) 2013 no320.com. All rights reserved.
+
+
+## 目录
+
+共7个目录
+
+	app    config   db     doc    log    script   test   vendor   server
+	
+
+### app    
+这个应用开发的源码目录和图片资源目录
+
+	./app:
+	assets      controllers helpers     models      views
+
+
+- assets    
+资源文件目录，默认只有images目录（如果需要js，css，html等都可以放到此目录）
+
+- controllers 
+UIViewController子类
+
+- helpers     
+工具类
+
+- models      
+于db，表单有关的的模型
+
+- views
+UIView的子类
+
+
+### config
+配置文件目录
+
+比如通过json配置显示某些UIView，实现可配置化
+   
+### db
+sqlite3数据库
+     
+### doc    
+文档目录
+
+appledoc和md格式的文档，可以生成docset
+
+### log    
+日志目录
+
+
+### script   
+常用脚本，命令
+
+
+### test   
+暂时用默认的sentest（ghunit，spec）
+
+
+### server
+采用sinatra做静态json测试
+
+
+### vendor
+外部引用的资源库，建议用git的submodule来处理。
+
+比如：
+
+	git submodule add  https://github.com/i5ting/IOS-TEST-UNIT.git gwcl/vendor/ios-test-unit
+
+
+
+## 欢迎fork
---
+++ init.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+#
+#  init.sh
+#  create ios project dir like rails dir
+#
+#  Created by sang alfred on 3/6/13.
+#  Copyright (c) 2013 no320.com. All rights reserved.
+# 
+
+# ➜  test git:(master) ✗ ls
+# app    config db     doc    log    script test   vendor
+# ➜  test git:(master) ✗ ls -R
+# app    config db     doc    log    script test   vendor
+# 
+# ./app:
+# assets      controllers helpers     models      views
+# 
+# ./app/assets:
+# images
+# 
+# ./app/assets/images:
+# 
+# ./app/controllers:
+# 
+# ./app/helpers:
+# 
+# ./app/models:
+# 
+# ./app/views:
+# 
+# ./config:
+# environments initializers locales
+# 
+# ./config/environments:
+# 
+# ./config/initializers:
+# 
+# ./config/locales:
+# 
+# ./db:
+# 
+# ./doc:
+# 
+# ./log:
+# 
+# ./script:
+# 
+# ./test:
+# 
+# ./vendor:
+#
+#
+#
+#
+
+mkdir -p app/assets/images
+mkdir -p app/controllers 
+mkdir -p app/helpers        
+mkdir -p app/models      
+mkdir -p app/views
+
+mkdir db
+mkdir doc
+mkdir vendor
+mkdir log
+mkdir test
+mkdir script
+
+mkdir -p config/environments
+mkdir -p config/initializers
+mkdir -p config/locales
+
