edad8c982bbbb20db4a6cd5bbe012162c9592a26
i5ting <i5ting@126.com>
2013-04-09 22:48:12 +0800

    Update init.sh
    modify

--- init.sh
+++ init.sh
@@ -58,10 +58,10 @@ mkdir -p src/app/controllers
 mkdir -p src/app/helpers        
 mkdir -p src/app/models      
 mkdir -p src/app/views
+mkdir -p src/app/apis
 
 mkdir src/db
 mkdir src/doc
-mkdir src/vendor
 mkdir src/log
 mkdir src/test
 mkdir src/script
@@ -71,5 +71,8 @@ mkdir -p src/config/environments
 mkdir -p src/config/initializers
 mkdir -p src/config/locales
 
-sqlite3 src/db/test.db ""
+sqlite3 src/db/gospel_ios.db ""
 
+mkdir vendor
+
+touch src/doc/README.md
